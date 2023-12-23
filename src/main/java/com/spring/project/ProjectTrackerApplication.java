477b122f57be071a94342630dc21250c66cc30c1
Prathamesh Moharkar <prathameshmoharkar125@gmail.com>
2023-12-24 00:07:31 +0530

    authentication bug solved

--- src/main/java/com/spring/project/ProjectTrackerApplication.java
+++ src/main/java/com/spring/project/ProjectTrackerApplication.java
@@ -1,26 +1,46 @@
 package com.spring.project;
 
+import java.util.Optional;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.mongodb.client.MongoClient;
+import com.spring.auth.Role;
+import com.spring.auth.User;
+import com.spring.project.dao.UserRepository;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @ComponentScan(basePackages = "com.spring")
 public class ProjectTrackerApplication {
 
 	private static MongoClient mongoClient;
+
+	@Autowired UserRepository userRepository;
+	@Autowired PasswordEncoder passwordEncoder;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectTrackerApplication.class, args);
-		
-		
-		
 	}
-	
 
-
+	@PostConstruct
+	public void init(){
+		System.out.println("Working");
+        Optional<User> fetchedUser = userRepository.findByUserName("admin");
+        System.out.println(fetchedUser + " data before if loadeed ");
+        if(!fetchedUser.isPresent()){
+            User owner = new User();
+            owner.setPassword(passwordEncoder.encode("adminPassword"));
+            owner.setRole(Role.OWNER);
+            owner.setUserName("admin");
+            userRepository.save(owner);
+            System.out.println("data saved in database");
+        }
+	}
 }
--- src/main/java/com/spring/project/controller/AuthController.java
+++ src/main/java/com/spring/project/controller/AuthController.java
@@ -1,13 +1,14 @@
 package com.spring.project.controller;
+import java.util.Optional;
 
-import java.io.Console;
-import java.util.Optional; 
+import javax.swing.text.html.Option;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus; 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager; 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken; 
 import org.springframework.security.core.Authentication; 
@@ -25,11 +26,11 @@ import com.spring.auth.User;
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/auth")
-@PropertySource("classpath:application.properties")
+// @PropertySource("classpath:application.properties")
 public class AuthController { 
 	
 	@Autowired 
-	private UserService service ; 
+	private UserService userService ; 
 	
 	@Autowired 
 	private JwtService jwtService ; 
@@ -42,58 +43,86 @@ public class AuthController {
 	
 	
 
-    @Value("${owner.username}")
-    private String ownerUsername;
+    // @Value("${owner.username}")
+    // private String ownerUsername;
 
-    @Value("${owner.password}")
-    private String ownerPassword;
+    // @Value("${owner.password}")
+    // private String ownerPassword;
 	
-    @PostMapping("/login")
-    public AuthResponse authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
-        // Check if the user is the owner with hardcoded credentials
-        if (authRequest.getUserName().equals(ownerUsername) && authRequest.getPassword().equals(ownerPassword)) {
-            // If the provided credentials match the owner's credentials
-            String token = jwtService.generateToken(authRequest.getUserName());
+    // @PostMapping("/login")
+    // public AuthResponse authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+    //     // Check if the user is the owner with hardcoded credentials
+    //     if (authRequest.getUserName().equals(ownerUsername) && authRequest.getPassword().equals(ownerPassword)) {
+    //         // If the provided credentials match the owner's credentials
+    //         String token = jwtService.generateToken(authRequest.getUserName());
 
-            AuthResponse authResponse = new AuthResponse();
-            authResponse.setToken(token);
-            authResponse.setRole("OWNER");  // Set the role as per your role enum
-            authResponse.setUsername(authRequest.getUserName());
-            System.out.println(authResponse.getUsername() + " logged in..........");
+    //         AuthResponse authResponse = new AuthResponse();
+    //         authResponse.setToken(token);
+    //         authResponse.setRole("OWNER");  // Set the role as per your role enum
+    //         authResponse.setUsername(authRequest.getUserName());
+    //         System.out.println(authResponse.getUsername() + " logged in..........");
             
 
-            return authResponse;
-        } else {
-            // Proceed with regular authentication logic for non-owner users
-            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword()));
+    //         return authResponse;
+    //     } else {
+    //         // Proceed with regular authentication logic for non-owner users
+    //         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword()));
 
-            if (authentication.isAuthenticated()) {
-                Optional<User> fetchedUser = userRepo.findByUserName(authRequest.getUserName());
+    //         if (authentication.isAuthenticated()) {
+    //             Optional<User> fetchedUser = userRepo.findByUserName(authRequest.getUserName());
 
-                if (fetchedUser.isPresent()) {
-                    String role = fetchedUser.get().getRole().toString();
-                    String token = jwtService.generateToken(authRequest.getUserName());
+    //             if (fetchedUser.isPresent()) {
+    //                 String role = fetchedUser.get().getRole().toString();
+    //                 String token = jwtService.generateToken(authRequest.getUserName());
 
-                    AuthResponse authResponse = new AuthResponse();
-                    authResponse.setToken(token);
-                    authResponse.setRole(role);
-                    authResponse.setUsername(authRequest.getUserName());
+    //                 AuthResponse authResponse = new AuthResponse();
+    //                 authResponse.setToken(token);
+    //                 authResponse.setRole(role);
+    //                 authResponse.setUsername(authRequest.getUserName());
 
-                    return authResponse;
-                } else {
-                    throw new UsernameNotFoundException("Invalid user request!");
-                }
-            } else {
-                throw new UsernameNotFoundException("Invalid User Request");
+    //                 return authResponse;
+    //             } else {
+    //                 throw new UsernameNotFoundException("Invalid user request!");
+    //             }
+    //         } else {
+    //             throw new UsernameNotFoundException("Invalid User Request");
+    //         }
+    //     }
+    // }
+
+
+    @PostMapping("/login")
+    public AuthResponse authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        String username = authRequest.getUserName();
+        String password = authRequest.getPassword();
+
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        if(authentication.isAuthenticated()){
+            Optional<User> fetchedUser = userRepo.findByUserName(username);
+            if(fetchedUser.isPresent()){
+                String role = fetchedUser.get().getRole().toString();
+                String token = jwtService.generateToken(authRequest.getUserName());
+                AuthResponse authResponse = new AuthResponse();
+                authResponse.setToken(token);
+                authResponse.setRole(role);
+                authResponse.setUsername(username);
+                return authResponse;
             }
+            throw new UsernameNotFoundException(username + " seq:1");
         }
+        else{
+            throw new UsernameNotFoundException(username + " seq:2");
+        }
+
     }
+    
+   
 	
 	@PostMapping("/register") 
 //	@PreAuthorize("hasAuthority('OWNER')")
 	public ResponseEntity<HttpStatus> createUser(@RequestBody User user) { 
 		System.out.println(user.toString());
-		User u = service.createTheUser(user) ; 
+		User u = userService.createTheUser(user) ; 
 		if(u!=null) 
 			return new ResponseEntity<>(HttpStatus.CREATED) ; 
 		else 
